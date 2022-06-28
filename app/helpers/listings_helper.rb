e0f2bcc13fb20b664722ec5d448be0e9802cc8ec
HardeepSinghAu <11919@coderacademy.edu.au>
2022-06-28 16:28:35 +1000

    listings condition presentation

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,10 @@
 module ListingsHelper
+
+    def format_condition(condition)
+        condition_words = condition.split("_").map do |word|
+            word.capitalize 
+        end
+        condition_words.join(" ")
+    end 
+
 end
