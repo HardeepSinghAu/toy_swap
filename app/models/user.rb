359e31aaa8df7649e961d6db6c39bcd28f135d1d
HardeepSinghAu <11919@coderacademy.edu.au>
2022-06-29 00:59:56 +1000

    set up foreign keys for orders

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :listings
+
+  has_many :sold_orders, foreign_key: "seller_id", class_name: "Order"
+  has_many :bought_orders, foreign_key: "buyer_id", class_name: "Order"
   
 
 end
